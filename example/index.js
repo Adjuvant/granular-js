35049419fa5d6544aece80561750a38ddd8d6ee4
adjuvant <tom@layeraudio.com>
2020-08-21 16:59:56 +0100

    muck about with example

--- example/index.js
+++ example/index.js
@@ -24,28 +24,30 @@ async function getData(url) {
 async function init() {
   const audioContext = p5.prototype.getAudioContext();
   
+  const timeInMillis = 30000;
   const granular = new Granular({
     audioContext,
     envelope: {
-      attack: 0,
+      attack: 0.1,
       decay: 0.5
     },
-    density: 0.9,
-    spread: 0.1,
+    density: 0.3,
+    spread: 0.2,
     pitch: 1
   });
 
   const delay = new p5.Delay();
 
   delay.process(granular, 0.5, 0.5, 3000); // source, delayTime, feedback, filter frequency
+  delay.drywet(0.1);
 
   const reverb = new p5.Reverb();
-
+  reverb.drywet(0.2); 
   // due to a bug setting parameters will throw error
   // https://github.com/processing/p5.js/issues/3090
-  reverb.process(delay); // source, reverbTime, decayRate in %, reverse
+  reverb.process(delay, 3, 5); // source, reverbTime, decayRate in %, reverse
 
-  reverb.amp(3);
+  reverb.amp(1);
 
   const compressor = new p5.Compressor();
 
@@ -55,7 +57,7 @@ async function init() {
   granular.on('bufferSet', () => console.log('buffer set'));
   granular.on('grainCreated', () => console.log('grain created'));
 
-  const data = await getData('example.wav');
+  const data = await getData('creed.mp3');
 
   await granular.setBuffer(data);
 
@@ -63,19 +65,35 @@ async function init() {
 
   resume.addEventListener('click', () => {
     const id = granular.startVoice({
-      position: 0.1,
+      position: 0.0,
       gain: 0.5
     });
 
     let pitch = 1;
+    let down = true;
+    let t_0 = performance.now();
 
     const interval = setInterval(() => {
-      pitch -= 0.05;
+      if(down)
+        pitch -= 0.15;
+      else 
+        pitch += 0.15;
 
+      if (pitch < 0.5 ||  pitch > 1.0) down = !down;
+      //console.log(pitch);
       granular.set({
         pitch
       });
-    }, 200);
+      
+      var t_1 = performance.now();
+      var dt = t_1 - t_0;
+
+      var pos = Math.clip(map (dt, 0, timeInMillis, 0.0, 1.0),0,1);
+      // console.log(pos);
+      granular.updateVoice(id, {
+        position: pos
+      });
+    }, 15);
 
     setTimeout(() => {
       clearInterval(interval);
@@ -85,8 +103,16 @@ async function init() {
       granular.set({
         pitch: 1
       });
-    }, 2000);
-  })
-}
+    }, timeInMillis);
+  }) // end resume listener
+} // end init
 
 init();
+
+function map(value, inMin, inMax, outMin, outMax) {
+  return (value - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+}
+
+Math.clip = function(number, min, max) {
+  return Math.max(min, Math.min(number, max));
+}
